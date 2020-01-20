c54ccf2558147e278d0fcfc79d0b194bfee36d86
martinyounghoonkim <hoons0131@gmail.com>
2020-01-20 20:31:58 +0900

    Repository Interface 생성 및 Controller에 post API 추가

--- src/main/java/com/springboot/application/domain/posts/PostsRepository.java
+++ src/main/java/com/springboot/application/domain/posts/PostsRepository.java
@@ -1,4 +1,10 @@
 package com.springboot.application.domain.posts;
 
-public class PostsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * MyBatis 등에서 Dao라고 불리는 DB Layer 접근자
+ * JPA에서는 Repository라고 부르며, 인터페이스로 생성한다.
+ */
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
--- src/main/java/com/springboot/application/web/WebRestController.java
+++ src/main/java/com/springboot/application/web/WebRestController.java
@@ -1,12 +1,24 @@
 package com.springboot.application.web;
 
+import com.springboot.application.domain.posts.PostsRepository;
+import com.springboot.application.dto.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+    private PostsRepository postsRepository;
     @GetMapping("/hello")
     public String hello() {
         return "Hello, this is Martin's tutorial.";
+    }
+
+    @PostMapping("/post")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
