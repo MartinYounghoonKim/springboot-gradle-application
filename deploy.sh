71ba29cc97a8578ef446b4e03c65a1e65ab1bb75
martinyounghoonkim <hoons0131@gmail.com>
2020-05-13 08:34:40 +0900

    배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-REPOSITORY=/home/ubuntu/app/git
-
-cd ${REPOSITORY}/springboot-gradle-application/
-
-echo "> Git Pull"
-
-git pull
-
-echo "> 프로젝트 Build 시작"
-
-./gradlew build
-
-echo "> Build 파일 복사"
-
-cp ./build/libs/*.jar ${REPOSITORY}/
-
-echo "> 현재 구동중인 애플리케이션 pid 확인"
+REPOSITORY=/home/ubuntu/app/travis
 
 CURRENT_PID=$(pgrep -f springboot-gradle-application)
 
@@ -25,8 +9,8 @@ echo "$CURRENT_PID"
 if [[ -z ${CURRENT_PID} ]]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -2 ${CURRENT_PID}"
-    kill -2 ${CURRENT_PID}
+    echo "> kill -15 ${CURRENT_PID}"
+    kill -15 ${CURRENT_PID}
     sleep 5
 fi
 
