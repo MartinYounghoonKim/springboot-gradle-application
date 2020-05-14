dfc6dd861a0a3b3b0800fc62573199a113113386
martinyounghoonkim <hoons0131@gmail.com>
2020-05-15 08:14:18 +0900

    배포 스크립트 수정

--- src/main/java/com/springboot/application/web/WebRestController.java
+++ src/main/java/com/springboot/application/web/WebRestController.java
@@ -14,7 +14,7 @@ public class WebRestController {
     private PostsService postsService;
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, this is Martin's tutorial. haha";
+        return "Hello, this is Martin's tutorial. ㅎㅎㅎㅎㅎ";
     }
 
     @GetMapping("/")
